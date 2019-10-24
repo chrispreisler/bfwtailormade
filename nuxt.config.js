dfe219b4a496158816641af1d109ebdf1633c03f
chrispreisler <hello@chrispreisler.com>
2019-10-24 15:29:22 +0200

    config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-const pkg = require('./package')
 const PrismicConfig = require('./prismic.config')
 
 module.exports = {
@@ -8,11 +7,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
