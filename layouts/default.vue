3872bf06a66da89c2309f41eeddd2e5f952e55a5
chrispreisler <hello@chrispreisler.com>
2019-10-24 12:07:09 +0200

    first

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header-bfw />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import HeaderBfw from '~/components/HeaderBfw.vue'
+
+export default {
+  components: {
+    HeaderBfw
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-center px-32 md:px-64 mx-auto">
+    <h1 class="text-6xl mt-64">Homepage</h1>
+    <p class="mt-16">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sed odio dui. Nullam id dolor id nibh ultricies vehicula ut id elit. Sed posuere consectetur est at lobortis.</p>
+  </div>
+</template>
