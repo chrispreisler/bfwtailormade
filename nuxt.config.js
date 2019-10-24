f7dea631bf6db5500c1787d0f25fd8467c352261
chrispreisler <hello@chrispreisler.com>
2019-10-24 15:43:18 +0200

    netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
-
+const pkg = require('./package')
+const PrismicConfig = require('./prismic.config')
 
 module.exports = {
   mode: 'universal',
@@ -7,24 +8,37 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:400,700,900,400italic,700italic' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lora:400,400italic,700,700italic' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
-    ]
+    ],
+    script: [
+      { innerHTML: '{ window.prismic = { endpoint: "' + PrismicConfig.apiEndpoint + '"} }' },
+      { src: '//static.cdn.prismic.io/prismic.min.js' }
+    ],
+    __dangerouslyDisableSanitizers: ['script'],
   },
 
   /*
   ** Customize the progress-bar color
   */
   loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/css/resetr.css',
+    '@/assets/css/common.css'
+  ],
 
   /*
   ** Plugins to load before mounting the App
@@ -35,24 +49,11 @@ module.exports = {
     '~/plugins/prismic-vue.js',
   ],
 
-  css: [
-    '@/assets/css/tailwind.css'
-  ],
-
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-
   /*
   ** Nuxt.js modules
   */
   modules: [
   ],
-
-  generate: {
-    "dir": "public"
-  },
 
   /*
   ** Build configuration
